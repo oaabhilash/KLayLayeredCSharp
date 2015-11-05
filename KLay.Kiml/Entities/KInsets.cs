56b47927b4cad0d05dbc1c5e02c2a838090650b3
oaabhilash <oaabhilash@hotmail.com>
2015-11-04 20:20:19 -0500

    adding the Kgraph data structure elements

---
+++ KLay.Kiml/Entities/KInsets.cs
@@ -0,0 +1,69 @@
+﻿using KLay.Kiml.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/// <summary>
+///KLAY:: de.cau.cs.kieler.kiml.klayoutdata.impl.KInsetsImpl 
+/// 
+/// Even though java code is implementing an interface, we are considering this as an entity
+/// </summary>
+
+namespace KLay.Kiml
+{
+    public class KInsets 
+    {
+        //KLAY:TOP_EDEFAULT
+        float _top = 0.0f;
+
+        //KLAY:BOTTOM_EDEFAULT
+        float _bottom = 0.0f;
+
+        //KLAY: LEFT_EDEFAULT
+        float _left = 0.0f;
+
+        //KLAY: RIGHT_EDEFAULT
+        float _right = 0.0f;
+
+        public float Top
+        {
+            get { return _top; }
+            set { _top = value; }
+        }
+
+        public float Bottom
+        {
+            get { return _bottom; }
+            set { _bottom = value; }
+        }
+
+        public float Left
+        {
+            get { return _left; }
+            set { _left = value; }
+        }
+
+        public float Right
+        {
+            get { return _right; }
+            set { _right = value; }
+        }
+
+
+        public String toString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append("(top: ");
+            sb.Append(_top);
+            sb.Append(", bottom: ");
+            sb.Append(_bottom);
+            sb.Append(", left: ");
+            sb.Append(_left);
+            sb.Append(", right: ");
+            sb.Append(_right);
+            sb.Append(")");
+            return sb.ToString();
+        }
+    }
+}
---
+++ KLay.Kiml/KShapeLayout.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/// <summary>
+///  de.cau.cs.kieler.kiml.klayoutdata.impl.KShapeLayoutImpl
+/// 
+/// </summary>
+namespace KLay.Kiml
+{
+    public class KShapeLayout
+    {
+        float _xpos = 0.0f; //KLAY: XPOS_EDEFAULT
+        float _ypos = 0.0f; //KLAY: YPOS_EDEFAULT
+        float _width = 0.0f; //KLAY: WIDTH_EDEFAULT
+        float _height = 0.0f; //KLAY: HEIGHT_EDEFAULT
+
+        
+
+        bool _modified = false;
+
+        public float XPos
+        {
+            get { return _xpos; }
+            set
+            {
+                _modified = (_xpos != value);
+                _xpos = value;
+            }
+        }
+
+        public float YPos
+        {
+            get { return _ypos; }
+            set
+            {
+                _modified = (_ypos != value);
+                _ypos = value;
+            }
+        }
+
+        public float Width
+        {
+            get { return _width; }
+            set
+            {
+                _modified = (_width != value);
+                _width = value;
+            }
+        }
+
+        public float Height
+        {
+            get { return _height; }
+            set
+            {
+                _modified = (_height != value);
+                _height = value;
+            }
+        }
+
+        public KInsets Insets
+        {
+            get; set;
+        }
+
+        public bool IsModified()
+        {
+            return _modified;
+        }
+
+        public void ResetModificationFlag()
+        {
+            _modified = false;
+        }
+
+    }
+}
